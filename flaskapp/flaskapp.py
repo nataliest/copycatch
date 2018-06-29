9c651d660665e5b797cfd84393d6aaf9dac79344
Ubuntu <ubuntu@ip-10-0-0-12.ec2.internal>
2018-06-29 03:17:51 +0000

    results page done

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -10,20 +10,37 @@ def index():
 
 @app.route('/stats/<id>')
 def display_detailed_info(id):
-	r = redis.StrictRedis(host='redis-db.7ptpwl.ng.0001.use1.cache.amazonaws.com', port=6379, db=4)
-	key = '/m/' + id
-	stuff = "Label is: " + r.get(key) + "!!"
-	return render_template("detailed.html", label = stuff)
+	r = redis.StrictRedis(host='redis-db.7ptpwl.ng.0001.use1.cache.amazonaws.com', port=6379, db=5)
+	all_keys = r.keys()
+        num_entries = len(all_keys)
+        not_all_keys = all_keys[:10]
+        out = []
+        #column_names = ['Labels', 'Total Images', 'Filtered Images', 'Redis Tag Retrieval (seconds)', 'Spark Image Processing (seconds)', 'Copy Found/Not Found', 'Structural Similarity']
+        column_names = ['labels','numtotal','filtered','redis','spark','total','found','ssim','url_orig','url_incom']
+        for key in all_keys:
+                d = {'Key': key}
+                val_list = r.lrange(key, 0, -1)
+                for i in range(len(column_names)):
+                        d[column_names[i]] = val_list[i]
+                out.append(d)
+        
+        return render_template("detailed.html", list_out=out)
 
 @app.route('/stats')
 def hello_world():
-	r = redis.StrictRedis(host='redis-db.7ptpwl.ng.0001.use1.cache.amazonaws.com', port=6379, db=4)
+	r = redis.StrictRedis(host='redis-db.7ptpwl.ng.0001.use1.cache.amazonaws.com', port=6379, db=5)
 	all_keys = r.keys()
-	num_entries = len(all_keys[:10])
+	num_entries = len(all_keys)
 	not_all_keys = all_keys[:10]
 	out = []
-	for key in not_all_keys:
-		out.append({"Key": key, "val": r.get(key)})
+	#column_names = ['Labels', 'Total Images', 'Filtered Images', 'Redis Tag Retrieval (seconds)', 'Spark Image Processing (seconds)', 'Copy Found/Not Found', 'Structural Similarity']
+	column_names = ['labels','numtotal','filtered','redis','spark','total','found','ssim']
+        for key in all_keys:
+		d = {'Key': key}
+		val_list = r.lrange(key, 0, -1)
+		for i in range(len(column_names)):
+			d[column_names[i]] = val_list[i]
+		out.append(d)
 	
 	return render_template("stats.html", list_out=out)
 
